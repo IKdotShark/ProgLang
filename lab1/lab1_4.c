9d48a7ea880d8cadb70878a30ceb20dd68d40ba8
Alex <46602606+IKdotShark@users.noreply.github.com>
2023-09-15 08:15:25 +0500

    Add files via upload

---
+++ lab1/lab1_4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    FILE *file = fopen (argv[1], "r");
+    int len = sizeof(file);
+    char arr[len];
+    if (file == NULL){
+        printf("ERROR of inputing file");
+        exit(EXIT_FAILURE);
+    }
+    while (fgets(arr, len, file) != NULL)
+    {
+        printf("%s\n", arr);
+    }
+    
+    fclose(file);
+
+    return 0;
+}
